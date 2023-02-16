a25d9e5586b94d51f9bd52ad44121b99ff557471
Kunal Dutta <98681937+KunalDutta03@users.noreply.github.com>
2023-02-16 10:43:07 +0530

    Add files via upload

---
+++ R/KunalD.R
@@ -0,0 +1,19 @@
+
+#' Create Histogram Boxplot and Numeric Summary
+#' @export
+hello <- function() {
+  print("Hello, world!")
+}
+
+
+kun<-function(x){
+  par(mfrow=c(1,2))
+  hist(x,col=rainbow(30))
+  boxplot(x,col="blue")
+  par(mfrow=c(1,1))
+  data.frame(min=min(x),
+            median=median(x),
+            max=max(x))
+}
+
+
---
+++ R/hello.R
@@ -0,0 +1,31 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+
+kun<-function(x){
+  par(mfrow=c(1,2))
+  hist(x,col=rainbow(30))
+  boxplot(x,col="blue")
+  par(mfrow=c(1,1))
+  data.frame(min=min(x),
+            median=median(x),
+            max=max(x))
+}
+
+
